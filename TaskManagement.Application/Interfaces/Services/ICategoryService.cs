94268891ed2d8f62f5ec52a17979b3fcf8c4d383
IsaacAdadu <isaacadaduoche@gmail.com>
2025-07-30 15:28:27 +0100

    Created services for category and Task

---
+++ TaskManagement.Application/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.DTOs.Category;
+
+namespace TaskManagement.Application.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDto>> GetAllAsync();
+    }
+}
---
+++ TaskManagement.Application/Interfaces/Services/ITaskService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.DTOs.Task;
+
+namespace TaskManagement.Application.Interfaces.Services
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskDto>> GetAllAsync(string category = null, string priority = null, bool? isCompleted = null);
+        Task<TaskDto> GetByIdAsync(int id);
+        Task CreateAsync(CreateTaskDto dto);
+        Task UpdateAsync(int id, UpdateTaskDto dto);
+        Task DeleteAsync(int id);
+        Task MarkAsCompletedAsync(int id);
+    }
+}
