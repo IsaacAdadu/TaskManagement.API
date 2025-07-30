a8bd1a7cc07995244b00f5989416b58bbe49f21c
IsaacAdadu <isaacadaduoche@gmail.com>
2025-07-30 15:13:17 +0100

    commit

---
+++ TaskManagement.Domain/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Domain.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task SaveChangesAsync();
+    }
+}
---
+++ TaskManagement.Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Domain.Interfaces.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync(string category = null, string priority = null, bool? isCompleted = null);
+        Task<TaskItem> GetByIdAsync(int id);
+        Task AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+        Task DeleteAsync(TaskItem task);
+        Task SaveChangesAsync();
+    }
+}
