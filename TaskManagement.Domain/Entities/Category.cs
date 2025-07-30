0d65b7ce964736da509727e914db6aedd4e2ff28
IsaacAdadu <isaacadaduoche@gmail.com>
2025-07-30 15:12:49 +0100

    Created my entities TaskItem and Category

---
+++ TaskManagement.Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public ICollection<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ TaskManagement.Domain/Entities/TaskItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Enums;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public PriorityLevel Priority { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime? DueDate { get; set; }
+    }
+}
