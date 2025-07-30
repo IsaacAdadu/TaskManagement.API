f14b98cd56238cc345f9c942c3296ba11b5f3be1
IsaacAdadu <isaacadaduoche@gmail.com>
2025-07-30 15:27:47 +0100

    Created Dtos for category and Task

---
+++ TaskManagement.Application/DTOs/Task/CreateTaskDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.DTOs.Task
+{
+    public class CreateTaskDto
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Priority { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ TaskManagement.Application/DTOs/Task/TaskDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.DTOs.Task
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Category { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
